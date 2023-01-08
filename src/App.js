b4dea6c5e37288a3f09dc522c757dc33718f7940
Mr._Theophilus <yeshua.t137@gmail.com>
2023-01-08 01:01:38 -0800

    My First React Movie Webpp.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import SearchIcon from './search.svg';
+import Movie_Card from './MovieCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const API_URL = 'http://www.omdbapi.com?apikey=c41da66d';
+
+const App = () => {
+
+    // WE'RE SETTING THE MOVIES STATE HERE
+    const [movies, setMovies] = useState([]);
+
+    // WE'RE SETTING THE SEARCH STATE HERE
+    const [search_Term, setSearchTerm] = useState('');
+
+
+    const search_Movies = async (title) => {
+        // The Code Below Calls The API
+        const response = await fetch(`${API_URL}&s=${title}`);
+
+        // We Get The Data From The Response Here
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        search_Movies('Spiderman');
+    }, []);
+    
+
+
+    return (
+        <div className='app'>
+            <h1>TYT Movie Station</h1>
+
+
+            {/* THIS IS THE SEARCH SECTION OF THE WEBAPP */}
+            <div className='search'>
+                <input
+                    placeholder='Search for movies'
+                    value={search_Term}
+                    onChange={(e) => setSearchTerm(e.target.value)} 
+                />
+                <img
+                    src={SearchIcon}
+                    // ALTERNATIVE('alt') TAG IS USED FOR SCREEN READERS
+                    alt='search'
+                    // THIS SAME ICON SERVES AS A BUTTON
+                    onClick={ () => {
+                            search_Movies(search_Term);
+                            alert(`Finding ${search_Term} Kindly Wait, Your Response Will Be Ready Soon`);
+                        }
+                    }
+                />
+            </div>
+
+            
+            {/* 
+                THIS IS THE MOVIE CONTAINER
+                CHECKING THE THEIR ARE MOVIES OR NOT 
+            */} 
+            {
+                movies?.length > 0
+                ? (
+                    <div className="container">
+                        {/* 
+                            WE'RE MAPPING OVER MOVIES IN EACH SECTION OF THE API RESPONSE,
+                            AND WE WILL RENDER A MOVIE CARD COMPONENT 
+                        */}
+                        {movies.map((movie) => (
+                            <Movie_Card movie={movie}/>
+                        ))}
+                    </div>
+                ) : (
+                    <div className='empty'>
+                        <h2>Sorry, no movie found</h2>
+                    </div>
+                )
+            }
+
+
+        </div>
+    );
 }
 
 export default App;
+ 
---
+++ src/MovieCard.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+// WE'RE CREATING A COMPONENT
+const Movie_Card = ({ movie }) => {
+
+    return (
+        <div className='movie'>
+
+            <div>
+                <p>{movie.Year}</p>
+            </div>
+
+            {/* THIS IS THE MOVIE IMAGE SECTION */}
+            <div>
+                <img 
+                    // IF THE API THATS NOT PROVIDE US ANY IMAGE,
+                    // THEN WE PROVIDE AN ERROR IMAGE TO THE USER 
+                    // 'N/A' IS HOW THE API DECLARES MOVIES WITH NO IMAGE
+                    src={ movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'}
+                    alt={movie.Title} 
+                />
+            </div>
+
+            {/* THIS IS THE MOVIE TYPE AND TITLE SECTION */}
+            <div>
+                <span>{movie.Type}</span>
+                <h3>{movie.Title}</h3>
+            </div>
+
+        </div>
+    )
+}
+
+export default Movie_Card;
